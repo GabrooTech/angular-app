917e1f84b13a6b3d0cd869576af442088c62c06d
GabrooTech <87397156+GabrooTech@users.noreply.github.com>
2024-03-03 17:39:09 +0400

    Update add-servers.component.ts

--- src/app/add-servers/add-servers.component.ts
+++ src/app/add-servers/add-servers.component.ts
@@ -98,7 +98,7 @@ export class AddServersComponent implements AfterViewInit{
           fileDataURL(file).then((dataUrl: string) => {
             const dominantColorNow = this.getDominantColor(image, ctx);
             this.dominant_color = dominantColorNow;
-            console.log('Dominant Color:', dominantColorNow); // Log the dominant color
+            console.log('Dominant Color:', dominantColorNow);
           });
         }
         image.src = readerEvent.target.result as string;
